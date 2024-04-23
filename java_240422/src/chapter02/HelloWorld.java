4dee97f65d8ce5328b61e96ae78fec890d1f8027
DohoonHan <hsm3809@naver.com>
2024-04-23 16:21:42 +0900

    24-04-23 수업

---
+++ java_240422/src/chapter02/HelloWorld.java
@@ -0,0 +1,15 @@
+package chapter02;
+
+/*
+ *  작성일 : 2024-04-23
+ *  작성자 : 한도훈
+ *  내용 : Chapter02, HelloWorld 클래스 문자열 출력
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		String message = "헬로 월드";
+		System.out.println(message);
+		message = "Hello World";
+		System.out.println(message);	
+	}
+}
